7f4d7da809c97bcd0cc910cb0e4c339f415ba953
Respect Murimi <respectmurimi2000@gmail.com>
2024-05-30 09:04:24 +0200

    Illustrate the use of floatind data type

---
+++ FloatingPointTypes.java
@@ -0,0 +1,11 @@
+package main.java.com.respect17.datatypes;
+
+public class FloatingPointTypes {
+    public static void main(String[] args) {
+        float floatVar = 10.5f;
+        double doubleVar = 20.5d;
+
+        System.out.println("Float Value: " + floatVar);
+        System.out.println("Double Value: " + doubleVar);
+    }
+}
