c7a072c02fac126be9d45a0d1c7d24931b9f1e09
Respect Murimi <respectmurimi2000@gmail.com>
2024-05-30 09:53:15 +0200

    Illustrate how booleans work in Java

---
+++ BooleanType.java
@@ -0,0 +1,11 @@
+package main.java.com.respect17.datatypes;
+
+public class BooleanType {
+
+    public static void main(String[] args) {
+        boolean booleanVar = true;
+
+        System.out.println("Boolean Value: " + booleanVar);
+    }
+
+}
