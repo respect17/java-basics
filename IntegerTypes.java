4f19881899a668de354c2f1eea71b7c5c8f17867
Respect Murimi <respectmurimi2000@gmail.com>
2024-05-30 08:59:57 +0200

    Show how integers work

---
+++ IntegerTypes.java
@@ -0,0 +1,15 @@
+package main.java.com.respect17.datatypes;
+
+public class IntegerTypes {
+    public static void main(String[] args) {
+        byte byteVar = 100;
+        short shortVar = 10000;
+        int intVar = 1000000;
+        long longVar = 10000000000L;
+
+        System.out.println("Byte Value: " + byteVar);
+        System.out.println("Short Value: " + shortVar);
+        System.out.println("Int Value: " + intVar);
+        System.out.println("Long Value: " + longVar);
+    }
+}
