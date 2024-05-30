218367bde38fb69cf2d972ff3ecf2d82ca81f070
Respect Murimi <respectmurimi2000@gmail.com>
2024-05-30 11:26:45 +0200

    Show how to use local variables in Java

---
+++ variables/LocalVariableExample.java
@@ -0,0 +1,10 @@
+package main.java.com.respect17.datatypes.variables;
+
+public class LocalVariableExample {
+    public static void main(String[] args) {
+
+        // local variable is declared inside a method/constuctor/block
+        int localVar = 10; // Local variable
+        System.out.println("Local Variable: " + localVar);
+    }
+}
