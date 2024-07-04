3c00739daeaa0ac17b7a971346422543ea2f4873
Respect Murimi <respectmurimi2000@gmail.com>
2024-07-04 14:47:35 +0200

    Create a sample on how to call methods in Java

---
+++ Methods/Sample.java
@@ -0,0 +1,11 @@
+package main.java.com.respect17.datatypes.variables;
+
+public class Sample {
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
