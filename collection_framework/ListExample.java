92ac3853ec5a8018e89db57eb1b8d7e221f37512
Respect Murimi <respectmurimi2000@gmail.com>
2024-10-12 20:04:31 +0200

    Add list interface work

---
+++ collection_framework/ListExample.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListExample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Orange");
+
+        System.out.println(list); // Output: [Apple, Banana, Orange]
+    }
+}
